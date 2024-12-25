3913170201ba770684f43e251103c73ee57d97b4
Lorenzo Cara <lorycara62@gmail.com>
2024-12-25 11:32:29 +0100

    Initial Commit

---
+++ GitSourceControl/GitSourceControlApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitSourceControlApp.swift
+//  GitSourceControl
+//
+//  Created by Lorenzo Cara on 25/12/24.
+//
+
+import SwiftUI
+
+@main
+struct GitSourceControlApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
