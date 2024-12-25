ac932ad557ef3e61128dd0a0cd92c3376cc7bc08
Lorenzo Cara <lorycara62@gmail.com>
2024-12-25 16:17:45 +0100

    [Clean] Added the notes about Swftful Commits

--- GitSourceControl/ContentView.swift
+++ GitSourceControl/ContentView.swift
@@ -5,6 +5,25 @@
 //  Created by Lorenzo Cara on 25/12/24.
 //
 
+/*
+ Swiftful commits:
+ 
+ Features:
+ [Feature]: Description of the feature
+ 
+ Bug in production:
+ [Patch] description of the patch
+ 
+ Bug not in production:
+ [Bug]: description of the bug
+ 
+ Mundane tasks:
+ [Clean] Description o0f the task
+ 
+ Relase:
+ [Relase] Description of relase
+ */
+
 import SwiftUI
 
 struct ContentView: View {
