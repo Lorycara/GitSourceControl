6bf4ad0fbcf46434ece41db16027d319f61b6ee2
Lorenzo Cara <lorycara62@gmail.com>
2024-12-26 08:39:40 +0100

    [feature] added homeview file and created a text

---
+++ GitSourceControl/HomeView.swift
@@ -0,0 +1,18 @@
+//
+//  HomeView.swift
+//  GitSourceControl
+//
+//  Created by Lorenzo Cara on 26/12/24.
+//
+import Foundation
+import SwiftUI
+
+public struct HomeView: View {
+
+    
+    public var body: some View {
+        Text("Home View")
+            .padding()
+            .foregroundStyle(.red)
+    }
+}
