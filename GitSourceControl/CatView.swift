2d66dc574cf06d3879d0cff8380e75b4b39571ec
Lorenzo Cara <lorycara62@gmail.com>
2024-12-26 07:00:48 +0100

    [Feature] Cat added into CatView

---
+++ GitSourceControl/CatView.swift
@@ -0,0 +1,18 @@
+//
+//  CatView.swift
+//  GitSourceControl
+//
+//  Created by Lorenzo Cara on 26/12/24.
+//
+
+import SwiftUI
+
+struct CatView: View {
+    var body: some View {
+        Image(systemName: "cat")
+    }
+}
+
+#Preview {
+    CatView()
+}
